7286e8a0af7d62ee073bb291bbc6ed2a1c759343
Mahmoud <mohmahmo@sheridancollege.ca>
2019-01-21 13:47:04 -0500

    new repo for student code

---
+++ src/githubb/Githubb.java
@@ -0,0 +1,17 @@
+
+package githubb;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public class Githubb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
